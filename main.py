2a80c598c6813744a23fcc31253da92131ce0e0c
obamzuro <oleg.bamzurov@gmail.com>
2019-06-19 23:14:45 +0300

    the thrill is gone

--- main.py
+++ main.py
@@ -5,69 +5,173 @@ import json
 import hashlib
 import bitstring
 import os
+import time
+import re
 
-def md5(fileEntity):
+RED = 0b1
+GREEN = 0b10
+BLUE = 0b100
+
+API_ID = "##############"
+
+def print_debug(string, color, is_debug=None):
+    if is_debug and debug_level == "y" or not is_debug:
+        if (color):
+            if (color & RED):
+                print("\033[1;31;40m", end="")
+            elif (color & GREEN):
+                print("\033[1;32;40m", end="")
+            elif (color & BLUE):
+                print("\033[1;34;40m", end="")
+        print(string, end="")
+        print("\033[0;39;49m")
+
+def md5(file_entity):
     hash_md5 = hashlib.md5()
-    for chunk in iter(lambda: fileEntity.read(4096), b""):
+    for chunk in iter(lambda: file_entity.read(4096), b""):
         hash_md5.update(chunk)
+    file_entity.seek(0)
     return hash_md5.hexdigest()
 
-def upload_file_on_server(filename, file_hash, fileEntity):
+def response_1001_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    combined_verdict = json_data["response"]["te"]["combined_verdict"]
+    if combined_verdict == "malicious":
+        print_debug("File is malicious", RED)
+    elif combined_verdict == "benign":
+        print_debug("File is not malicious", GREEN)
+
+def response_1002_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    print_debug("File was uploaded successfully", GREEN)
+    te_record = json_data["response"]["te"]
+    if te_record.get("combined_verdict"):
+        if combined_verdict == "malicious":
+            print_debug("!File is malicios!", RED)
+        elif combined_verdict == "benign":
+            print_debug("!File is not malicios!", GREEN)
+    else:
+        check_file_on_server_uploaded(filename, file_entity, file_hash, te_cookie)
+
+def response_1003_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    print_debug("Request is pending", BLUE)
+    time.sleep(3)
+
+def response_1004_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    print_debug("File on server didn't found", RED)
+    if first_time:
+        upload_file_on_server(filename, file_entity, file_hash, te_cookie)
+
+def response_1006_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    print_debug("File on server didn't found", RED)
+    if first_time:
+        upload_file_on_server(filename, file_entity, file_hash, te_cookie)
+
+def response_1007_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    print_debug("File type is illegal.", RED)
+
+def response_1008_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    print_debug("Request format is not valid. Make sure the request follows this documentation.", RED)
+
+def response_1009_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    print_debug("There is a temporary error with the service. Try again in few minutes.", RED)
+
+def response_1010_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    print_debug("Forbidden query request", RED)
+
+def response_1011_status_code(filename, file_entity, file_hash, te_cookie, json_data, first_time):
+    print_debug("There is a temporary error with the service. Try again in few seconds.", RED)
+
+status_code_switcher = {
+        1001: response_1001_status_code,
+        1002: response_1002_status_code,
+        1003: response_1003_status_code,
+        1004: response_1004_status_code,
+        1006: response_1006_status_code,
+        1007: response_1007_status_code,
+        1008: response_1008_status_code,
+        1009: response_1009_status_code,
+        1010: response_1010_status_code,
+        1011: response_1011_status_code
+    }
+
+def check_file_on_server_uploaded(filename, file_entity, file_hash, te_cookie=None):
+    url = "https://te.checkpoint.com/tecloud/api/v1/file/query"
+    if te_cookie:
+        headers = {"Authorization": API_ID, "te_cookie": te_cookie}
+    else:
+        headers = {"Authorization": API_ID}
+    print_debug("Query server for uploaded file", BLUE)
+    params = { "request": {"md5": file_hash, "file_name": os.path.basename(filename)}}
+    while 1:
+        response = requests.post(url, data=json.dumps(params), headers=headers)
+        if (response.status_code == 400):
+            print_debug("400 Server Response error (check extension of file)", RED)
+        elif (response.status_code == 503):
+            print_debug("503 Service Unavailable", BLUE)
+            time.sleep(3)
+            file_entity.seek(0)
+            upload_file_on_server(filename, file_entity, file_hash, te_cookie)
+        print_debug(response.content.decode('ascii'), None, 1)
+        json_data = response.json()
+        first_time = False
+        if not te_cookie:
+            te_cookie = dict(re.findall(r'(\w+)=([^ ]+);', response.headers.get("Set-Cookie"))).get("te_cookie")
+            print_debug("te_cookie = " + te_cookie, None, 1)
+            first_time = True
+        status_code = json_data['response']['status']['code']
+        status_code_switcher.get(status_code)(filename, file_entity, file_hash, te_cookie, json_data, first_time)
+        # for pendig request
+        if (status_code == 1003):
+            continue
+        break
+
+def upload_file_on_server(filename, file_entity, file_hash, te_cookie):
     url = "https://te.checkpoint.com/tecloud/api/v1/file/upload"
     params = {
-            "request": 
+                "request": 
                 {
-                    "md5": file_hash, "file_name": filename, "file_type": "txt", "features": ["te"], "te":
-                    {
-                        "reports": ["summary"],
-                        "images": 
-                        [{
-                            "id": "e50e99f3-5963-4573-af9e-e3f4750b55e2",
-                            "revision": 1
-                        }]
-                    }
+                    "md5": file_hash, "file_name": os.path.basename(filename), "file_type": "application/octet-stream"
                 }
             }
-    headers = {"Authorization": "#######"}
+    headers = {"Authorization": API_ID, "te_cookie": te_cookie}
     files = {
-            'json': (None, json.dumps(params), 'application/json'),
-            'file': (os.path.basename(filename), fileEntity)
+                'json': (None, json.dumps(params), 'application/json'),
+                'file': (os.path.basename(filename), file_entity, "application/octet-stream")
             }
-    print("\033[1;34;40mUpload file to server\033[0;37;40m")
-    responce = requests.post(url, files=files, headers=headers);
-    print(responce.content.decode('ascii'))
-#    responce = requests.post(url, headers=headers, files=files);
-#    print(responce.content)
+    print_debug("Upload file to server", BLUE)
+    response = requests.post(url, files=files, headers=headers);
+    if (response.status_code == 400):
+        print_debug("400 Server Response error (check extension of file)", RED)
+    elif (response.status_code == 503):
+        print_debug("503 Service Unavailable", BLUE)
+        time.sleep(3)
+        file_entity.seek(0)
+        upload_file_on_server(filename, file_entity, file_hash, te_cookie)
+    json_data = response.json()
+    json_data = response.json()
+    print_debug(response.content.decode('ascii'), None, 1)
+    status_code = json_data['response']['status']['code']
+    status_code_switcher.get(status_code)(filename, file_entity, file_hash, te_cookie, json_data, None)
 
-def check_file_on_server(filename, isUploaded):
-    url = "https://te.checkpoint.com/tecloud/api/v1/file/query"
-    headers = {"Authorization": "#########"}
-    print("\033[1;34;40mQuery server if file has already exists\033[0;37;40m")
+def check_file_on_server(filename):
     try:
-        with open(filename, "rb") as fileEntity:
-            file_hash = md5(fileEntity)
-            params = { "request": {"md5": file_hash, "file_name": filename }}
-            responce = requests.post(url, data=json.dumps(params), headers=headers)
-            json_data = responce.json()
-            print(responce.content.decode('ascii'))
-            status_code = json_data['response']['status']['code']
-            if status_code == 1001:
-                print("\033[1;32;40mFile was found\033[0;37;40m")
-                upload_file_on_server(filename, file_hash, fileEntity)
-            elif status_code == 1004:
-                print("\033[1;31;40mFile on server didn't found\033[0;37;40m")
-                upload_file_on_server(filename, file_hash, fileEntity)
-    except FileNotFoundError as fnf_error:
-        print(fnf_error)
+        with open(filename, "rb") as file_entity:
+            file_hash = md5(file_entity)
+            check_file_on_server_uploaded(filename, file_entity, file_hash)
+    except json.decoder.JSONDecodeError as json_error:
+        print_debug("json_response convert error", RED)
+    except Exception as e:
+        print_debug(e, RED)
 
-def debug_http():
-    http.client.HTTPConnection.debuglevel = 1
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    requests_log.propagate = True
+def debug_http(debug_level):
+    if (debug_level == "y"):
+        http.client.HTTPConnection.debuglevel = 1
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.DEBUG)
+        requests_log.propagate = True
 
-debug_http()
 filename = input("Type the name of the file to be checked:\n")
-check_file_on_server(filename, 0)
+debug_level = input("Is debug needed? (y/N) ")
+debug_http(debug_level)
+check_file_on_server(filename)
